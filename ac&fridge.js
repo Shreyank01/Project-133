a9082db97afcb9483b81db70729d0652abad8f8f
Shreyank01 <87267295+Shreyank01@users.noreply.github.com>
2022-04-30 09:39:04 +0530

    Add files via upload

---
+++ ac&fridge.js
@@ -0,0 +1,44 @@
+function setup() {
+    canvas = createCanvas(600,400);
+    canvas.center();
+    object_detector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("result").innerHTML = "Total Object Detected by CocoSSD: Detecting...";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded !");
+    status = true;
+    object_detector.detect(img , gotResult);
+}
+
+function gotResult(error , results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage("ac & fridge.jpg");
+}
+
+function draw() {
+    image(img ,0 ,0 ,600 ,400); 
+
+    if(status != "") {
+        for(i=0; i < objects.length; i++) {
+            document.getElementById("result").innerHTML = "Total Object Detected by CocoSSD: " + objects.length;
+            
+               
+        }
+            
+    }
+
+}
